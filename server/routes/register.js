3521b76a24e8565ceaa9a0e870e7d2bbee13f654
zehan12 <zehan9211@gmail.com>
2024-12-12 00:22:43 +0530

    refactor: :tada: rewrite whole codebase with react router 7 with server component

--- server/routes/register.js
+++
@@ -1,9 +0,0 @@
-const registerRouter = require("express").Router();
-const { handleCreateRegisterUser } = require("../controllers/register.controllers");
-
-//=================================
-//  Register and create a User
-//=================================
-registerRouter.post("/", handleCreateRegisterUser )
-
-module.exports = registerRouter;
