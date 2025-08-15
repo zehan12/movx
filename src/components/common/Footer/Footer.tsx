fe9e2d356b332016ffbb3ba00e2ce919754cea82
zehan12 <zehan9211@gmail.com>
2025-08-15 15:48:39 +0530

    feat: :memo: add custom fonts

---
+++ src/components/common/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';;
+
+const Footer: React.FC = () => (
+  <div className="footer">
+    <span>
+      Made with <span className="text-red-500">❤️</span> by{" "}
+      <a href="https://zehankhan.vercel.app/" target="_blank">
+        Zehan Khan
+      </a>
+    </span>
+  </div>
+);
+
+export default Footer;
